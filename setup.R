8bc95cd77b09b01b80145e9eaea6146feaa8ca78
shaylatriant <shayla.triantafillou@colostate.edu>
2025-02-26 14:30:06 -0700

    Initial commit. Code includes parts 1 (getting NHD data) and 2 (snapping user-input points to NHD flowlines)

---
+++ setup.R
@@ -0,0 +1,27 @@
+# Script for installing (if needed) and loading packages for this project
+  # Adapted from Katie Willi (github: kathryn-willi)
+
+packageLoad <- function(x) {
+  for (i in 1:length(x)) {
+    if (!x[i] %in% installed.packages()) {
+      install.packages(x[i])
+    }
+    library(x[i], character.only = TRUE)
+  }
+}
+
+# create a string of package names
+
+packages <- c('here',
+              'tidyverse',
+              'nhdplusTools',
+              'ggthemes',
+              'sp',
+              'sf',
+              'prettymapr',
+              'rosm',
+              # 'mapview',
+              'terra')
+# use the packageLoad function we created on those packages
+
+packageLoad(packages)
